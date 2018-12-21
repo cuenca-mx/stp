6d741aaac99cd6531cf98ee1c1ba2d95651bf7a3
Matin Tamizi <matin@cuenca.com>
2018-12-21 12:44:55 -0600

    minor cleanup

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='stpmex',
-    version='1.0',
+    version='1.0.0',
     author='Cuenca',
     author_email='dev@cuenca.io',
     description='Integration to stpmex.com',
